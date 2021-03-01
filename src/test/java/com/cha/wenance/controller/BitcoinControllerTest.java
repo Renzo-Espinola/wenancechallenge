617379c1d04305e56ce9f93ab75116086e79f8c7
Renzo-Espinola <renzo.espinola87@gmail.com>
2021-02-28 23:46:37 -0300

    ***PRY WENANCE CHALLENGE*** -Update Controller

---
+++ src/test/java/com/cha/wenance/controller/BitcoinControllerTest.java
@@ -0,0 +1,24 @@
+package com.cha.wenance.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitcoinControllerTest {
+
+    @Test
+    void saveBitcoin() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void getAverageBitcoins() {
+    }
+
+    @Test
+    void getVarPorcBitcoins() {
+    }
+}
